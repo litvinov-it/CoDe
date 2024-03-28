66160276c2188bebaea2e7bbeaba4a5942ab2913
litvinov-it <litan.yt.terr@gmail.com>
2024-03-28 10:57:47 +0700

    v1.1.1 bigfix with layout navigation

--- src/components/modules/header/header.jsx
+++ src/components/modules/header/header.jsx
@@ -3,30 +3,12 @@ import { ReactComponent as Logo } from "../../../sprites/icons/logo.svg";
 import { BurgerMenu } from "../../components/burgerMenu/burger-menu";
 import { useState } from "react";
 import { Nav } from "../../components/nav/nav";
-import { useRef } from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
 
 export const Header = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
-  const containerRef = useRef();
-
-  useGSAP(
-    () => {
-      gsap.from(containerRef.current, {
-        y: -100,
-        opacity: 0,
-        duration: 1,
-        scrollTrigger: {
-          trigger: containerRef.current
-        },
-      });
-    },
-    { scope: containerRef }
-  );
 
   return (
-    <header ref={containerRef} className={classes.header}>
+    <header className={classes.header}>
       <div className={classes.wrapper}>
         <Logo className={classes.logo} />
         <Nav isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} />
